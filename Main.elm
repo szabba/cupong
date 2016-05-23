4e992bcb7d204390af2018171fd9114774edc998
Karol Marcjan <karol.marcjan@gmail.com>
2016-05-24 00:12:12 +0200

    Add crude 3D

--- Main.elm
+++ Main.elm
@@ -5,11 +5,11 @@
 
 module Main exposing (..)
 
-import Html exposing (Attribute)
+import Html as H exposing (Attribute)
 import Html.App as App
-import Html.Attributes as AHtml
-import Svg exposing (Svg)
-import Svg.Attributes as ASvg
+import Html.Attributes as HA
+import Svg as S exposing (Svg)
+import Svg.Attributes as SA
 import Task
 import Window
 import Bouncy
@@ -33,6 +33,7 @@ main =
 type alias Model =
     { window : WindowSize
     , bouncy : Bouncy.Model
+    , backScale : Float
     }
 
 
@@ -46,19 +47,24 @@ init : ( Model, Cmd Message )
 init =
     let
         withEmptyWindow bouncy =
-            { window =
+            { bouncy = bouncy
+            , backScale = 1 / 2
+            , window =
                 { width = 0
                 , height = 0
                 }
-            , bouncy = bouncy
             }
 
         ( model, cmd, _ ) =
-            Bouncy.init { radius = 10, halfDiagonal = Vector 300 300 300 }
+            Bouncy.init
+                { radius = 25
+                , halfDiagonal = Vector 400 300 400
+                }
     in
         (model |> withEmptyWindow)
             ! [ cmd |> Cmd.map BouncyMsg
-              , Task.perform (\_ -> Debug.crash "initial window size") Resize Window.size
+              , Window.size
+                    |> Task.perform (\_ -> Debug.crash "initial window size") Resize
               ]
 
 
@@ -98,79 +104,130 @@ update msg model =
 
 
 view : Model -> Svg msg
-view { window, bouncy } =
-    wrapWindow window
-        [ viewBox bouncy.box
-        , viewSphere bouncy.ball
-        ]
+view { window, bouncy, backScale } =
+    let
+        scale =
+            { backScale = backScale }
+    in
+        wrapWindow window
+            [ viewBox scale bouncy.box
+            , viewBall scale bouncy.box bouncy.ball
+            ]
 
 
 wrapWindow : WindowSize -> List (Svg msg) -> Svg msg
 wrapWindow { width, height } elements =
     let
         baseAttributes =
-            [ ASvg.version "1.1"
-            , ASvg.baseProfile "full"
-            , ASvg.width <| toString <| width
-            , ASvg.height <| toString <| height
-            , AHtml.attribute "xmlns" "http://www.w3.org/2000/svg"
+            [ SA.version "1.1"
+            , SA.baseProfile "full"
+            , SA.width <| toString <| width
+            , SA.height <| toString <| height
+            , HA.attribute "xmlns" "http://www.w3.org/2000/svg"
             ]
 
         size =
-            [ ASvg.width <| toString <| width
-            , ASvg.height <| toString <| height
+            [ SA.width <| toString <| width
+            , SA.height <| toString <| height
             ]
 
         styling =
-            [ AHtml.style
+            [ HA.style
                 [ (,) "background" background
                 , (,) "display" "block"
                 ]
             ]
 
         centering =
-            [ ASvg.transform <| "translate(" ++ toString (width // 2) ++ " " ++ toString (height // 2) ++ ")" ]
+            [ SA.transform <| "translate(" ++ toString (width // 2) ++ " " ++ toString (height // 2) ++ ")" ]
     in
-        Svg.svg (baseAttributes ++ size ++ styling) [ Svg.g centering elements ]
+        S.svg (baseAttributes ++ size ++ styling) [ S.g centering elements ]
 
 
-viewSphere : Bouncy.Ball -> Svg msg
-viewSphere ball =
+viewBall : { backScale : Float } -> Bouncy.Box -> Bouncy.Ball -> Svg msg
+viewBall { backScale } box ball =
     let
+        zMax =
+            abs box.halfDiagonal.z
+
+        -- scale z = a * z + b
+        -- scale (-zMax) = 1
+        -- scale zMax = backScale
+        scale =
+            let
+                ( a, b ) =
+                    ( (backScale - 1) / (2 * zMax), (backScale + 1) / 2 )
+            in
+                a * ball.sphere.at.z + b
+
         ( cx, cy ) =
             ( .x, .y )
-                |> both ((|>) ball.sphere.at >> round >> toString)
-    in
-        Svg.circle
-            [ ASvg.cx cx
-            , ASvg.cy cy
-            , ASvg.r <| toString <| round <| ball.sphere.radius
-            , stroke
-            , strokeWidth
-            , fill
+                |> both ((|>) ball.sphere.at >> (*) scale >> round >> toString)
+
+        geometry =
+            [ SA.cx cx
+            , SA.cy cy
+            , SA.r <| toString <| round <| (*) scale <| ball.sphere.radius
             ]
-            []
-
-
-viewBox : Bouncy.Box -> Svg msg
-viewBox box =
-    let
-        param f attr component =
-            attr <| toString <| f <| abs <| component <| box.halfDiagonal
-
-        start =
-            param negate
-
-        dim =
-            param ((*) 2)
-
-        shape =
-            [ start ASvg.x .x, start ASvg.y .y, dim ASvg.width .x, dim ASvg.height .y ]
 
         style =
-            [ stroke, strokeWidth, ASvg.fillOpacity "0" ]
+            [ stroke, strokeWidth, fill ]
     in
-        Svg.rect (shape ++ style) []
+        S.circle (geometry ++ style) []
+
+
+viewBox : { backScale : Float } -> Bouncy.Box -> Svg msg
+viewBox { backScale } box =
+    let
+        rect scale =
+            { width = round <| scale * 2 * abs box.halfDiagonal.x
+            , height = round <| scale * 2 * abs box.halfDiagonal.y
+            }
+
+        ( front, back ) =
+            ( rect 1, rect backScale )
+
+        rects =
+            [ front, back ] |> List.map centeredRect
+
+        line xSign ySign =
+            S.line
+                [ SA.x1 <| toString <| xSign <| front.width // 2
+                , SA.y1 <| toString <| ySign <| front.height // 2
+                , SA.x2 <| toString <| xSign <| back.width // 2
+                , SA.y2 <| toString <| ySign <| back.height // 2
+                , stroke
+                , strokeWidth
+                ]
+                []
+
+        lines =
+            [ line identity identity
+            , line identity negate
+            , line negate identity
+            , line negate negate
+            ]
+    in
+        S.g [] (rects ++ lines)
+
+
+centeredRect : { width : Int, height : Int } -> Svg msg
+centeredRect { width, height } =
+    let
+        geometry =
+            [ SA.x <| toString <| negate <| flip (//) 2 <| width
+            , SA.y <| toString <| negate <| flip (//) 2 <| height
+            , SA.width <| toString <| width
+            , SA.height <| toString <| height
+            ]
+
+        style =
+            [ stroke
+            , strokeWidth
+            , SA.fillOpacity "0"
+            ]
+    in
+        S.rect (geometry ++ style) []
 
 
 both : (a -> b) -> ( a, a ) -> ( b, b )
@@ -180,17 +237,17 @@ both f ( x, y ) =
 
 fill : Attribute msg
 fill =
-    ASvg.fill background
+    SA.fill background
 
 
 stroke : Attribute msg
 stroke =
-    ASvg.stroke "#000000"
+    SA.stroke "#000000"
 
 
 strokeWidth : Attribute msg
 strokeWidth =
-    ASvg.strokeWidth <| toString 5
+    SA.strokeWidth <| toString 1
 
 
 background : String
